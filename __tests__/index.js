359b0ac5bd2451e94c03377c445e904fefa204da
Erik Langille <eriklangille@gmail.com>
2024-12-31 21:34:54 -0500

    ea statement its own test

--- __tests__/index.js
+++ __tests__/index.js
@@ -19,27 +19,29 @@ async function getFile(name) {
   }
 }
 
-async function compare(query) {
-  await zql.exec(query);
-  const sqlQuery = sql.prepare(query);
-  const sqlResult = sqlQuery.all();
-  // console.log(zqlResult);
-  expect(zqlResult.length).toBe(sqlResult.length);
-  for (let i = 0; i < sqlResult.length; i++) {
-    const obj = sqlResult[i];
-    let j = 0;
-    for (const key in obj) {
-      const sqlItem = obj[key];
-      let zqlItem = zqlResult[i][j];
-      if (typeof zqlItem == 'bigint') {
-        zqlItem = Number(zqlItem);
+function compare(query) {
+  test(query, async () => {
+    await zql.exec(query);
+    const sqlQuery = sql.prepare(query);
+    const sqlResult = sqlQuery.all();
+    // console.log(zqlResult);
+    expect(zqlResult.length).toBe(sqlResult.length);
+    for (let i = 0; i < sqlResult.length; i++) {
+      const obj = sqlResult[i];
+      let j = 0;
+      for (const key in obj) {
+        const sqlItem = obj[key];
+        let zqlItem = zqlResult[i][j];
+        if (typeof zqlItem == 'bigint') {
+          zqlItem = Number(zqlItem);
+        }
+        expect(zqlItem).toBe(sqlItem);
+        j++;
       }
-      expect(zqlItem).toBe(sqlItem);
-      j++;
     }
-  }
-  // clear the array
-  zqlResult = [];
+    // clear the array
+    zqlResult = [];
+  });
 }
 
 async function load(dbFile) {
@@ -54,41 +56,50 @@ async function load(dbFile) {
   console.log("Loaded");
 }
 
-describe('Compare ZQL to SQL', () => {
-  test('small db', async () => {
-    await load(SMALL_DB_FILE)
-    await compare("select * from example;");
-    await compare("select id, name from example;");
-    await compare("select id from example;");
-    await compare("select name from example;");
-    await compare("select * from example where id = 1;");
-    await compare("select * from example where name = 'Alice';");
-    await compare("select * from example where name = 'Alice' or name = 'Bob';");
-    await compare("select * from example where name like 'A%'");
-    await compare("select * from example where name like '%l%'");
+describe('small db', () => {
+  beforeAll(async () => {
+    await load(SMALL_DB_FILE);
   });
-  test('med db', async () => {
-    await load(MED_DB_FILE)
-    await compare("select * from t1;");
-    await compare("select * from t1 where name like 'a%' or name = 'Louis' or name = 'Paul';");
-    await compare("select * from t1 where age < 18 or name like '%a%';");
-    await compare("select * from t1 where age <= 18 or name like '%a%';");
-    await compare("select * from t1 where name like '_%';");
-    await compare("select * from t1 where name like '%_';");
-    await compare("select * from t1 where name like '%___';");
+  compare("select * from example;");
+  compare("select id, name from example;");
+  compare("select id from example;");
+  compare("select name from example;");
+  compare("select * from example where id = 1;");
+  compare("select * from example where name = 'Alice';");
+  compare("select * from example where name = 'Alice' or name = 'Bob';");
+  compare("select * from example where name like 'A%'");
+  compare("select * from example where name like '%l%'");
+});
+
+describe('med db', () => {
+  beforeAll(async () => {
+    await load(MED_DB_FILE);
   });
-  test('backup db', async () => {
-    await load(BACKUP_DB_FILE)
-    await compare("select * from test;");
+  compare("select * from t1;");
+  compare("select * from t1 where name like 'a%' or name = 'Louis' or name = 'Paul';");
+  compare("select * from t1 where age < 18 or name like '%a%';");
+  compare("select * from t1 where age <= 18 or name like '%a%';");
+  compare("select * from t1 where name like '_%';");
+  compare("select * from t1 where name like '%_';");
+  compare("select * from t1 where name like '%___';");
+});
+
+describe('backup db', () => {
+  beforeAll(async () => {
+    await load(BACKUP_DB_FILE);
   });
-  test('example db', async () => {
-    await load(BIG_DB_FILE)
-    await compare("select * from records where name like '%a';");
-    await compare("select * from records where name like '%a%a';");
-    await compare("select * from records where name like 'a%a';");
-    await compare("select * from records where name like 'a%b';");
-    await compare("select * from records where name like 'a%';");
-    await compare("select * from records where name like 'simfyvo__a';");
+  compare("select * from test;");
+});
+
+describe('example db', () => {
+  beforeAll(async () => {
+    await load(BIG_DB_FILE);
   });
+  compare("select * from records where name like '%a';");
+  compare("select * from records where name like '%a%a';");
+  compare("select * from records where name like 'a%a';");
+  compare("select * from records where name like 'a%b';");
+  compare("select * from records where name like 'a%';");
+  compare("select * from records where name like 'simfyvo__a';");
 });
 
